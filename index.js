21a61e80c5d50f16707ece9319e3416d710cb0c4
Drew Stokes <drew@drewstokes.com>
2013-06-30 11:35:15 -0700

    fix scope issue

--- index.js
+++ index.js
@@ -13,27 +13,25 @@ function Multicaster(options) {
   }
   EventEmitter.call(this);
 
+  var self = this;
   options = (options || {});
   this.port = (options.port || 3000);
-  this.listener = dgram.createSocket('udp4', this._handleMessage);
-  this.listener.bind(this.port);
+  this.socket = dgram.createSocket('udp4');
+  this.socket.on('message', this.onMessage.bind(this));
+  this.socket.on('error', function(e) { this.emit('error', e) });
+  this.socket.bind(this.port);
 }
 
-Multicaster.prototype._handleMessage = function(msg, src) {
+Multicaster.prototype.onMessage = function(msg, src) {
   src = (src || {});
   // filter out messages from this process
-  if(src.address === myip) return;
-
+  if(src.address === myip && src.port === this.port) return;
   this.emit('message', msg, src);
 }
 
 Multicaster.prototype.broadcast = function(msg, options) {
-  if(! this.emitter) {
-    this.emitter = dgram.createSocket('udp4');
-  }
-
   options = (options || {});
-  var port = (this.port || options.port);
+  var port = (options.port || this.port);
   var address = (options.address || '224.0.0.1');
 
   // try to encode message
@@ -46,10 +44,9 @@ Multicaster.prototype.broadcast = function(msg, options) {
     }
   }
   msg = new Buffer(msg);
-  this.emitter.send(msg, 0, msg.length, port, address)
+  this.socket.send(msg, 0, msg.length, port, address)
 }
 
 Multicaster.prototype.destroy = function() {
-  if(this.listener) this.listener.close();
-  if(this.emitter) this.emitter.close();
+  if(this.socket) this.socket.close();
 }
